04d34610dc62f3aef9055ad44d9954143a439806
Ajay Wankhade <ajaysw45@gmail.com>
2025-01-23 18:01:28 +0530

    secured jwt and dburl in env file

--- src/api/db/database.py
+++ src/api/db/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5432/product_db"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL= os.getenv("SQLALCHEMY_DATABASE_URL")
 
 # Create the database engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
